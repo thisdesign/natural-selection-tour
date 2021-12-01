6f2adb9129a9a5157e2d943a40b3b78c6bda2044
Jesse Ward <jesseward@jessedev.lan>
2021-12-01 15:39:04 -0800

    styling two column sections

--- components/layout/TwoColumn.vue
+++ components/layout/TwoColumn.vue
@@ -9,7 +9,26 @@
         :number="sidebarSectionNumber"
         :title="sidebarSectionTitle"
       />
-      <span>{{ sidebarStatus }}</span>
+      <div class="status" :class="statusColor">
+        <span class="status-title">{{ statusTitle }}</span>
+        <svg
+          class="status-icon"
+          viewBox="0 0 100 100"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle fill="currentColor" cx="50" cy="50" r="50" />
+          <text
+            x="50"
+            y="50"
+            text-anchor="middle"
+            dominant-baseline="middle"
+            font-size="450%"
+            font-family="Natural-Selection"
+          >
+            P
+          </text>
+        </svg>
+      </div>
       <slot name="footer"></slot>
       <!-- <prismic-rich-text :field="slice.primary.SidebarFooterText" /> -->
     </div>
@@ -40,5 +59,80 @@ export default {
       default: '',
     },
   },
+  data() {
+    return {
+      activeClass: 'blue',
+    }
+  },
+  computed: {
+    statusTitle() {
+      return this.sidebarStatus.replace('-', ' ')
+    },
+    statusColor() {
+      if (this.sidebarStatus === 'terrain-prep') {
+        return 'yellow'
+      } else if (this.sidebarStatus === 'live') {
+        return 'blue'
+      } else if (this.sidebarStatus === 'archived') {
+        return 'smoke-blue'
+      } else {
+        return ''
+      }
+    },
+  },
+  methods: {},
 }
 </script>
+
+<style lang="scss" scoped>
+section {
+  @include media-breakpoint-up(sm) {
+    display: flex;
+  }
+}
+.col {
+  flex: 1 1 auto;
+}
+.col-left {
+  @include media-breakpoint-up(sm) {
+    width: 75%;
+    padding-right: 2rem;
+  }
+}
+.col-right {
+  @include media-breakpoint-up(sm) {
+    width: 25%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+}
+.status {
+  margin: 0 auto auto 0;
+}
+.status-title {
+  display: block;
+  text-transform: capitalize;
+  line-height: 1;
+  font-size: 16vw;
+  margin-bottom: 22vw;
+  @include media-breakpoint-up(sm) {
+    font-size: 4vw;
+    margin-bottom: 3vw;
+    word-spacing: 10vw;
+  }
+}
+.status-icon {
+  margin-bottom: auto;
+  width: 100%;
+}
+.yellow {
+  color: $yellow-primary;
+}
+.blue {
+  color: $blue-primary;
+}
+.smoke-blue {
+  color: $blue-smoke;
+}
+</style>
--- slices/CountdownSlice/index.vue
+++ slices/CountdownSlice/index.vue
@@ -8,16 +8,41 @@
       :sidebar-section-title="slice.primary.SidebarSectionTitle"
     >
       <div class="countdown">
-        <prismic-rich-text :field="slice.primary.title" />
-        <span>{{ slice.primary.TargetDate }}</span>
-        <element-cta-button
-          :link="slice.primary.CtaLink"
-          :title="slice.primary.CtaTitle"
+        <prismic-rich-text
+          class="countdown-title"
+          :field="slice.primary.title"
         />
-        <prismic-rich-text :field="slice.primary.FooterContent" />
+        <div class="countdown-time">
+          <div class="countdown-row">
+            <span>{{ timer.days }}</span>
+            <span class="countdown-designation">Days</span>
+          </div>
+          <div class="countdown-row">
+            <span>{{ timer.hours }}</span>
+            <span class="countdown-designation">Hrs</span>
+          </div>
+          <div class="countdown-row">
+            <span>{{ timer.minutes }}</span>
+            <span class="countdown-designation">Min</span>
+          </div>
+        </div>
+        <div class="cta-row">
+          <element-cta-button
+            class="cta-button"
+            :link="slice.primary.CtaLink"
+            :title="slice.primary.CtaTitle"
+          />
+          <prismic-rich-text
+            class="last-updated"
+            :field="slice.primary.FooterContent"
+          />
+        </div>
       </div>
       <template #footer>
-        <prismic-rich-text :field="slice.primary.SidebarFooterText" />
+        <prismic-rich-text
+          class="two-column-footer"
+          :field="slice.primary.SidebarFooterText"
+        />
       </template>
     </layout-two-column>
   </div>
@@ -35,11 +60,108 @@ export default {
       },
     },
   },
+  data() {
+    return {
+      timer: {
+        days: '00',
+        hours: '00',
+        minutes: '00',
+      },
+    }
+  },
+  mounted() {
+    this.countdown()
+  },
+  methods: {
+    countdown() {
+      setInterval(() => {
+        const times = {}
+        const eventDate = new Date(this.slice.primary.TargetDate)
+        const currentTime = Date.now()
+        let deltaTime = Math.abs(eventDate - currentTime) / 1000
+        const timeUnitsInSeconds = {
+          days: 86400,
+          hours: 3600,
+          minutes: 60,
+        }
+
+        Object.keys(timeUnitsInSeconds).forEach((key) => {
+          times[key] = Math.floor(deltaTime / timeUnitsInSeconds[key])
+          deltaTime -= times[key] * timeUnitsInSeconds[key]
+        })
+
+        this.timer = times
+      }, 1000)
+    },
+  },
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .section {
   color: white;
+}
+.countdown-title {
+  h1 {
+    line-height: 0.9;
+    font-size: 8vw;
+  }
+}
+.countdown-time {
+  font-family: 'Natural-Selection';
+  line-height: 0.8;
+  font-size: 40vw;
+  @include media-breakpoint-up(sm) {
+    font-size: 10vw;
+    padding-bottom: 10vw;
+  }
+}
+.countdown-row {
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  text-transform: uppercase;
+  @include media-breakpoint-up(sm) {
+    justify-content: flex-start;
+  }
+}
+.countdown-designation {
+  display: block;
+  margin-left: 5%;
+  font-size: 0.1em;
+  font-family: 'Sneak';
+  @include media-breakpoint-up(sm) {
+    font-family: 'Natural-Selection';
+    font-size: 1em;
+  }
+}
+.cta-row {
+  display: flex;
+  flex-direction: column-reverse;
+  @include media-breakpoint-up(sm) {
+    flex-direction: row;
+  }
+}
+.cta-button {
+  text-align: center;
+  margin: 20vw 0 10vw;
+  @include media-breakpoint-up(sm) {
+    margin: 0 25% 0 0;
+    width: 50%;
+  }
+}
+.two-column-footer {
+  font-family: 'Sneak';
+  text-transform: uppercase;
+  margin-top: 10vw;
+  @include media-breakpoint-up(sm) {
+    margin-top: 0;
+  }
+}
+.last-updated {
+  margin-top: 10vw;
+  @include media-breakpoint-up(sm) {
+    margin-top: 0;
+  }
 }
 </style>
