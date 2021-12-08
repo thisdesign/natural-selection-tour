8ede44ac7d7bd5245552db9dc28c1fda2c086ac9
Jesse Ward <jesseward@jessedev.local>
2021-12-07 16:13:20 -0800

    mobile footer

---
+++ components/Footer/Copy.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>© 2022 Natural Selection Tour, Inc.<br />All rights reserved.</p>
+  </div>
+</template>
+<style lang="scss" scoped>
+* {
+  font-family: 'Sneak', sans-serif;
+  font-size: clamp(10px, 1vw, 12px);
+  text-transform: uppercase;
+}
+</style>
---
+++ components/Footer/FeatureText.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="feature-text">
+    <p>Created with stoke from remote regions across the earth️.</p>
+  </div>
+</template>
+<style lang="scss" scoped>
+.feature-text {
+  max-width: 250px;
+  p {
+    font-family: $baseFontFamily;
+    font-size: clamp(16px, 1.5vw, 20px);
+    line-height: 1;
+  }
+}
+</style>
