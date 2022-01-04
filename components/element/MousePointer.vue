9d4a6e17998c24aaf2c979490346c6369ccc369e
Jesse Ward <jesseward@jessedev.local>
2022-01-04 09:51:00 -0800

    refactor mousepointer to use as component

---
+++ components/element/MousePointer.vue
@@ -0,0 +1,107 @@
+<template>
+  <div
+    ref="mousePointer"
+    class="mouse-pointer"
+    :class="{ active: showPointer }"
+  >
+    <div class="inner">
+      <span class="pointer"></span>
+      <div class="name-wrapper">
+        <transition name="vt-fade" mode="out-in">
+          <span v-if="line1" :key="line1" class="rider-name">
+            <span>{{ line1 }}</span>
+            <br />
+            <span v-if="line2">{{ line2 }}</span>
+            <br />
+            <span v-if="line3">{{ line3 }}</span>
+          </span>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    showPointer: {
+      type: Boolean,
+      default: false,
+    },
+    line1: {
+      type: String,
+      default: null,
+    },
+    line2: {
+      type: String,
+      default: null,
+    },
+    line3: {
+      type: String,
+      default: null,
+    },
+  },
+  data() {
+    return {}
+  },
+  mounted() {
+    window.addEventListener('mousemove', this.onMouseMove)
+  },
+  methods: {
+    onMouseMove(event) {
+      if (this.$refs.mousePointer) {
+        this.$refs.mousePointer.style.transform = `translate(${event.pageX}px, ${event.pageY}px)`
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.mouse-pointer {
+  position: fixed;
+  z-index: 1000;
+  cursor: none;
+  pointer-events: none;
+  top: -0.5rem;
+  left: -0.5rem;
+  opacity: 0;
+  transition: opacity 300ms, transform 150ms ease-out;
+  min-width: max-content;
+  &.active {
+    opacity: 1;
+  }
+  * {
+    cursor: none;
+    pointer-events: none;
+  }
+  .inner {
+    display: flex;
+    align-items: flex-start;
+  }
+  .pointer {
+    background: white;
+    width: 1rem;
+    border-radius: 1rem;
+    height: 1rem;
+    margin-right: 1rem;
+    display: block;
+  }
+  .name-wrapper {
+    flex: 1 1 calc(100% - 2rem);
+    position: relative;
+  }
+  .rider-name {
+    font-family: 'Natural-Selection', sans-serif;
+    color: white;
+    line-height: 0.8;
+    font-size: 2vw;
+    display: table-caption;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    white-space: nowrap;
+  }
+}
+</style>
