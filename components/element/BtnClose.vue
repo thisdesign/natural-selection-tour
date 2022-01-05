9ce756d28c123d12fc2c19f4a8a81cc8450e3f2c
Jesse Ward <jesseward@jessedev.local>
2022-01-04 16:00:06 -0800

    slicing bracket

---
+++ components/element/BtnClose.vue
@@ -0,0 +1,47 @@
+<template>
+  <button class="close-modal-btn" @click="handleClick">
+    <span></span><span></span>
+  </button>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleClick() {
+      this.$emit('click')
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.close-modal-btn {
+  position: absolute;
+  right: 0;
+  top: 0;
+  margin-top: 0.7rem;
+  margin-right: 0.7rem;
+  width: 1rem;
+  height: 1rem;
+  background: none;
+  border: none;
+  padding: 0;
+  transform: rotate(45deg);
+  &:focus {
+    outline: none;
+  }
+  span {
+    border-radius: 10px;
+    display: block;
+    height: 2px;
+    width: 100%;
+    background: currentColor;
+    &:first-child {
+      transform: rotate(90deg) translate(1px, 0);
+    }
+    &:last-child {
+      transform: translate(0, -1px);
+    }
+  }
+}
+</style>
--- slices/BracketSlice/index.vue
+++ slices/BracketSlice/index.vue
@@ -7,48 +7,30 @@
         :title="slice.primary.SectionTitle"
       />
     </div>
-    <div class="bracket-columns site-padding">
-      <div class="bracket-column">
-        <div class="bracket-set">
-          <div class="bracket-rider">
-            <div class="rider-info">
-              <h3 class="rider-name">Sage Kotsenburg</h3>
-              <img src="/mocks/flag-mock.jpg" alt="" />
-            </div>
-            <div class="rider-ratings">
-              <div class="rating">90.5</div>
-              <div class="rating">93.9</div>
-            </div>
+    <div
+      ref="bracket"
+      class="bracket-columns site-padding"
+      @mousedown="mouseDownHandler"
+      @mousemove="mouseMoveHandler"
+      @mouseup="mouseUpHandler"
+    >
+      <div v-for="(round, index) in rounds" :key="index" class="bracket-column">
+        <div
+          v-for="(heat, heatIndex) in round.heats"
+          :key="heatIndex"
+          class="bracket-set"
+        >
+          <div class="connector-line"></div>
+          <bracket-athlete
+            v-for="(rider, atheleteIndex) in heat.athletes"
+            :key="atheleteIndex"
+          />
+          <div class="bracket-lines">
+            <span class="line"></span>
+            <span class="line"></span>
+            <span class="line"></span>
+            <span class="line"></span>
           </div>
-          <div class="bracket-rider"></div>
-        </div>
-        <div class="bracket-set">
-          <div class="bracket-rider"></div>
-          <div class="bracket-rider"></div>
-        </div>
-        <div class="bracket-set">
-          <div class="bracket-rider"></div>
-          <div class="bracket-rider"></div>
-        </div>
-        <div class="bracket-set">
-          <div class="bracket-rider"></div>
-          <div class="bracket-rider"></div>
-        </div>
-      </div>
-      <div class="bracket-column">
-        <div class="bracket-set">
-          <div class="bracket-rider"></div>
-          <div class="bracket-rider"></div>
-        </div>
-        <div class="bracket-set">
-          <div class="bracket-rider"></div>
-          <div class="bracket-rider"></div>
-        </div>
-      </div>
-      <div class="bracket-column">
-        <div class="bracket-set">
-          <div class="bracket-rider"></div>
-          <div class="bracket-rider"></div>
         </div>
       </div>
     </div>
@@ -67,6 +49,99 @@ export default {
       },
     },
   },
+  data() {
+    return {
+      rounds: [
+        {
+          heats: [
+            {
+              athletes: [1, 2],
+            },
+            {
+              athletes: [1, 2],
+            },
+            {
+              athletes: [1, 2],
+            },
+            {
+              athletes: [1, 2],
+            },
+            {
+              athletes: [1, 2],
+            },
+            {
+              athletes: [1, 2],
+            },
+            {
+              athletes: [1, 2],
+            },
+            {
+              athletes: [1, 2],
+            },
+          ],
+        },
+        {
+          heats: [
+            {
+              athletes: [1, 2],
+            },
+            {
+              athletes: [1, 2],
+            },
+            {
+              athletes: [1, 2],
+            },
+            {
+              athletes: [1, 2],
+            },
+          ],
+        },
+        {
+          heats: [
+            {
+              athletes: [1, 2],
+            },
+            {
+              athletes: [1, 2],
+            },
+          ],
+        },
+        {
+          heats: [
+            {
+              athletes: [1, 2],
+            },
+          ],
+        },
+      ],
+      grabbing: false,
+      scrollPosition: {
+        left: 0,
+        x: 0,
+      },
+    }
+  },
+  mounted() {
+    window.bracket = this.$refs.bracket
+  },
+  methods: {
+    mouseDownHandler(e) {
+      this.grabbing = true
+      this.$refs.bracket.style.cursor = 'grabbing'
+      this.scrollPosition.left = this.$refs.bracket.scrollLeft
+      this.scrollPosition.x = e.clientX
+    },
+    mouseMoveHandler(e) {
+      if (this.grabbing) {
+        const dx = e.clientX - this.scrollPosition.x
+        this.$refs.bracket.scrollLeft = this.scrollPosition.left - dx
+      }
+    },
+    mouseUpHandler() {
+      this.grabbing = false
+      this.$refs.bracket.style.cursor = 'grab'
+    },
+  },
 }
 </script>
 
@@ -77,46 +152,95 @@ export default {
 }
 .bracket-columns {
   display: flex;
-  overflow: scroll;
+  overflow: hidden;
+  cursor: grab;
+}
+.bracket-lines {
+  position: absolute;
+  right: 0;
+  top: 20%;
+  height: 60%;
+  span {
+    position: absolute;
+    height: 1px;
+    width: 1.5rem;
+    background: white;
+    display: block;
+    &:first-child {
+      top: 0;
+      width: 1.5rem;
+    }
+    &:nth-child(2) {
+      top: 0;
+      height: 100%;
+      width: 1px;
+      transform: translateX(1.5rem);
+    }
+    &:nth-child(3) {
+      top: 0;
+    }
+    &:last-child {
+      bottom: 0;
+    }
+  }
+}
+.bracket-set {
+  margin-bottom: 1rem;
+  position: relative;
 }
 .bracket-column {
   display: flex;
   flex-direction: column;
   justify-content: space-around;
-  margin-right: 1rem;
-}
-.bracket-set {
-  margin-bottom: 1rem;
-}
-.bracket-rider {
-  width: 43vw;
-  background: rgba(41, 41, 41, 0.76);
-  border: 1px solid #ffffff;
-  height: 5rem;
-  display: flex;
-  margin-bottom: 0.5rem;
-}
-.rider-info {
-  display: flex;
-  align-items: center;
-  width: 65%;
-  padding: 0.5rem;
-}
-.rider-name {
-  font-family: 'Natural-Selection', sans-serif;
-  font-size: 2vw;
-  line-height: 0.7;
-  margin-bottom: 0;
-}
-.rider-ratings {
-  display: flex;
-  width: 35%;
-  .rating {
-    flex: 1 1 auto;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-left: 1px solid white;
+  margin-right: 3rem;
+
+  &:first-child {
+    .bracket-set {
+      &:nth-child(odd) {
+        .bracket-lines span.line {
+          &:last-child {
+            width: 3rem;
+          }
+        }
+      }
+      &:nth-child(even) {
+        .bracket-lines span.line {
+          &:first-child {
+            width: 3rem;
+          }
+        }
+      }
+    }
+  }
+
+  &:not(:first-child) {
+    .bracket-set span.line {
+      &:nth-child(3) {
+        top: 50%;
+        transform: translateX(100%);
+      }
+    }
+  }
+
+  &:last-child {
+    .bracket-set span.line {
+      display: none;
+    }
+  }
+  &:nth-child(n + 3) {
+    .connector-line {
+      width: 1px;
+      height: 384px;
+      top: 50%;
+      transform: translateY(-50%);
+      background: white;
+      position: absolute;
+    }
+  }
+  &:nth-child(n + 4) {
+    .connector-line {
+      height: 768px;
+    }
   }
 }
 </style>
