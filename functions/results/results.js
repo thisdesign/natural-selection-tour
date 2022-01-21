0468f3217237502b8d6817011b76cadaf08beabe
Steve Wanless <steve@14four.com>
2022-01-21 13:42:55 -0800

    removed google key and repleaced it with a env var

--- functions/results/results.js
+++ functions/results/results.js
@@ -4,12 +4,12 @@ const storagePackage = require('@google-cloud/storage')
 
 const { Storage } = storagePackage;
 const { EVENTBRITE_TOKEN } = process.env
+const { GOOGLE_KEY } = process.env
 
-const creds = {
+let creds = {
   "type": "service_account",
   "project_id": "spring-gift-336003",
-  "private_key_id": "1bda4a8dfe72ef225570fa2b3edca88564c4f4a4",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDF5nyqIoaD+rF1\nCqSzRfdrRXMkdgTc+PRc17LhWTePt10omkbFJa0g0O8xrBymR8iyl55J+AAn7m2h\nghvdtcTa7GtYSaWPzFnXO+Aa6f2ToU251JHB/+Mmg39FhAi3bsFvCJkm5LaIXPZc\nCpufOJLEGM6xOzZTzz/HWr+3oMJmaj2LbNxibVhmiIBs+A2/DGJplEp7CGj5MEpS\nbKVVLY+pgu+UY/+tGPyj4KpLexXTwhNh/UdIungzErGF/OfQ18E+19x6XLkhBMLC\nb1rFpmJPGgpmI7+ycrcNJOEbDa1hHERpJEPFzEq+wBb1QMp0euRZB4p4WtIe9p5o\nK1S0nJvXAgMBAAECggEAB/FRUimMX4vcunDtnYC/ea+YYiXwc22OFS0J2C+5LwxY\nPoYINOc0Gx73mQ7dn+WkQVkcRVi6qaxqlWXzUcMqJIucXupzYCGYZRVZab5Ravu5\nP5RiyZ5h0szKFPndhbGTk63zKgbAHE0q2a49T7cfQNddpR3v9ye+v6gKc/rAqQgk\np+fuoJOBsKWN8MXG8SwYa3MJItyV3UnI8GTtNs4cGj097VXBqoXD0Sz0nivbHaQ5\ng8S65QYGtM2B/WmBisQDdw+j3tAbYrYJXIPIFrlbQL0hRpIHal9+uyRpsRwoIuRO\nE7llSwkrS31LBPlO8YuAWMUu9uL54/ZwJ3SL8Dy6TQKBgQDs2xzoaEZ9AoNZJbvZ\nlFHock140/w28q1XOfhd+eZ2YrQbn/TBXZc+vS+becVDuqWoQr4jKnOFeHOBc+Jx\nK5KHSt71lw/yLP99XjxtCEzbxsHoq9hzftz1kZC1hjc+UtmKERDN8XBFwWM9y1Sm\nrSO/5st774OBfD62KnlfZF6V+wKBgQDV5VPhvzeA1QrAQTL8Rto0tpPpzzZnWoeF\ndVZFnH6TFjBI+UN5py+aetOSHu6pPYbmFgaADGVmIwr8+09JlfcUTK+DmWHMzQJ4\ngLEzZXB1RGbYooa/HZuB56gPhQvhp2QzqOE6XAcUNzmV8+cvIa9Z1UCgtpwzWxUs\nn7GlvX7W1QKBgC/K5sJeiBqhby+F1anJq91jcw52d+X+gcuf/uW3jzFEKWOJmzGJ\nWQNn2TteDtfjyZlyHjuqTTUZYPU2FPMQ/7gc+3BC0wzi14rMgRTfH+MY7/eq9ToY\n1y0V+kUml+8TeKHLX8CufyEULJpDdQlM1nHaeCTB558xGtZXK7Z74joRAoGBAJLd\ndr4PdIXCQjKF0gNEhvkgz+j/fURz9Y+eZJFBhetqldbVSA5UzCvO4Ex5brfH5kM/\nplDvsgPyVc4kpGlLTi6jBPRgMbXUojipbIJ0yUXLpK5k0g6Rvr/wlwWo/eJ/9B5A\ne/19SOMJlEGxCfDjuSn+cfSnzhK5HI3aTLkXpurNAoGBAJ9QaS+UNNaVOM/h8TwX\nzzREhbiuTaz+eMwYzwtX1GY1SDpyYJbN8GLPLia7C1lpfTj4lkVhTpuPwjb4D0N/\nOS3yZR6ohkqn2aMWRhGjTGcdeMnAN9dlsmTkFrEKJoFxyUweEHu1MJO4JSlxVgqJ\nzS0pA+93iY4viO/ARAoxXcGf\n-----END PRIVATE KEY-----\n",
+  "private_key_id": "c9647f756ac86e1a069adf6d8d7f82a95dd48ce2",    
   "client_email": "results@spring-gift-336003.iam.gserviceaccount.com",
   "client_id": "110869427993518144962",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
@@ -17,6 +17,8 @@ const creds = {
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/results%40spring-gift-336003.iam.gserviceaccount.com"
 }
+  
+creds.private_key = GOOGLE_KEY
 
 const handler = async function (event) {
   try {
